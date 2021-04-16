c4bc413aa674af71ea9abc70602204b348665c16
Daniel Lins <daniel.alves.321@gmail.com>
2021-04-16 16:25:03 -0300

    nextjs-blog

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <article>
+        <h1>
+          404 - Page not found
+        </h1>
+      </article>
+    </Layout>
+  )
+}
